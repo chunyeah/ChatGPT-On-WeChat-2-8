1ae1d81b16f6581fa57d2fcc2981713b1dc27daf
Chun Ye <yechunjun@gmail.com>
2023-02-08 11:59:36 +0800

    Update chatgpt.ts

--- src/chatgpt.ts
+++ src/chatgpt.ts
@@ -12,7 +12,7 @@ const ChatGPTModelConfig = {
   // this model field is required
   model: "text-davinci-003",
   // add your ChatGPT model parameters below
-  temperature: 0.7,
+  temperature: 0.9,
   max_tokens: 3000,
 };
 
